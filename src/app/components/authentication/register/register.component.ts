c8af508a0c0f91a5802f38c4ce5e162938517d0f
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2024-01-04 21:30:52 -0500

    feat(authentication): adding phone input

--- src/app/components/authentication/register/register.component.ts
+++ src/app/components/authentication/register/register.component.ts
@@ -40,9 +40,41 @@ export class RegisterComponent implements OnInit {
     { viewValue: 'Uruguay' },
     { viewValue: 'Venezuela' }
   ];
+  isSelectBoxActive = false;
+  selectedCountryCode = 'pe'; // Replace with your default country code
+  selectedCountryPhone = '54'; // Replace with your default country phone
+  searchQuery = '';
+  countries = [
+    { code: '+54', name: 'Argentina', phone: 54 },
+    { code: '+55', name: 'Brazil', phone: 55 },
+    { code: '+56', name: 'Chile', phone: 56 },
+    { code: '+57', name: 'Colombia', phone: 57 },
+    { code: '+52', name: 'Mexico', phone: 52 },
+    { code: '+51', name: 'Peru', phone: 51 },
+    { code: '+598', name: 'Uruguay', phone: 598 },
+    // Agrega más países según sea necesario
+  ];
+
+
   constructor(public dialogRef: MatDialogRef<RegisterComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  get filteredCountries() {
+    return this.countries.filter(country =>
+      country.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  toggleSelectBox() {
+    this.isSelectBoxActive = !this.isSelectBoxActive;
+  }
+
+  selectOption(country: any) {
+    this.selectedCountryCode = country.code.toLowerCase();
+    this.selectedCountryPhone = country.phone;
+    this.isSelectBoxActive = false; // Close the dropdown after selection
   }
 
 }
