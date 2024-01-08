93d9430668dfb7e8b0105cd06fd1cf1a0f85ebfb
Anderson Ore Aleman <u202011467@upc.edu.pe>
2024-01-08 11:44:53 -0500

    feat: update premium
    - change color palette
    - update layout
    - add dynamic slider

--- src/app/components/premium/premium.component.ts
+++ src/app/components/premium/premium.component.ts
@@ -3,7 +3,16 @@ import KeenSlider, { KeenSliderInstance } from "keen-slider";
 
 const smallMediaQuery = window.matchMedia('(max-width: 770px)');
 const mediumMediaQuery = window.matchMedia('(min-width: 771px) and (max-width: 1500px)');
-
+export interface Course {
+  id: number;
+  title: string;
+  imgUrl: string;
+  teacher: string;
+  rating: string;
+  hours: string;
+  difficulty: string;
+  price: string;
+}
 
 @Component({
   selector: 'app-premium',
@@ -14,10 +23,58 @@ export class PremiumComponent{
   @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLElement>;
 
   slider: KeenSliderInstance | null = null;
-  currentSlide: number = 1
+  currentSlide: number = 1;
+
+
+  courses: Course[] = [
+    {
+      id: 1,
+      title: 'Curso de Angular',
+      imgUrl: 'https://cdn.discordapp.com/attachments/793721172285849600/1193239787172266094/image.png?ex=65abfe26&is=65998926&hm=b4e373db920520a5c1b41d0176ddbd431b514c44f98fcf31669ab923d8e8b6b8&',
+      teacher: 'Profesor Angular',
+      rating: '4.5',
+      hours: '30',
+      difficulty: 'Intermedio',
+      price: 'Free'
+    },
+    {
+      id: 2,
+      title: 'Curso de TypeScript',
+      imgUrl: 'https://cdn.discordapp.com/attachments/793721172285849600/1193239787172266094/image.png?ex=65abfe26&is=65998926&hm=b4e373db920520a5c1b41d0176ddbd431b514c44f98fcf31669ab923d8e8b6b8&',
+      teacher: 'Profesor TypeScript',
+      rating: '2.0',
+      hours: '20',
+      difficulty: 'Principiante',
+      price: 'Paid'
+    },
+    {
+      id: 3,
+      title: 'Curso de Angular',
+      imgUrl: 'https://cdn.discordapp.com/attachments/793721172285849600/1193239787172266094/image.png?ex=65abfe26&is=65998926&hm=b4e373db920520a5c1b41d0176ddbd431b514c44f98fcf31669ab923d8e8b6b8&',
+      teacher: 'Profesor Angular',
+      rating: '5',
+      hours: '30',
+      difficulty: 'Intermedio',
+      price: 'Free'
+    },
+    {
+      id: 4,
+      title: 'Curso de TypeScript',
+      imgUrl: 'https://cdn.discordapp.com/attachments/793721172285849600/1193239787172266094/image.png?ex=65abfe26&is=65998926&hm=b4e373db920520a5c1b41d0176ddbd431b514c44f98fcf31669ab923d8e8b6b8&',
+      teacher: 'Profesor TypeScript',
+      rating: '3.5',
+      hours: '20',
+      difficulty: 'Principiante',
+      price: 'Paid'
+    },
+  ];
+
   ngAfterViewInit() {
      this.initSlider()
   }
+
+  ngOnInit(): void {}
+
 
   private calculatePerView(): number {
     if (smallMediaQuery.matches) {
@@ -61,6 +118,9 @@ export class PremiumComponent{
     if (this.slider) this.slider.destroy();
   }
 
-  ngOnInit(): void {}
+  getRange(n: string): number[] {
+    const number = parseFloat(n);
+    return Array.from({ length: number }, (_, index) => index + 1);
+  }
 
 }
