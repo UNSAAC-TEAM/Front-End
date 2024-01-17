4e7e8fb477f6d0e7dad3f8e080fa87ddd2ff4acf
Anderson Ore Aleman <u202011467@upc.edu.pe>
2024-01-17 12:01:01 -0500

    feat(course): update course card content

--- src/app/modules/detail-course/components/detail-course-card/detail-course-card.component.ts
+++ src/app/modules/detail-course/components/detail-course-card/detail-course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-detail-course-card',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-course-card.component.css']
 })
 export class DetailCourseCardComponent implements OnInit {
+  hideImage = false;
 
-  constructor() { }
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    const matCardElement = this.el.nativeElement.querySelector('.mat-card');
+
+    const triggerHeight = 200;
+    const triggerFixed = 1700;
+    this.hideImage = window.scrollY > triggerHeight;
+    if (scrollPosition > triggerFixed) {
+      this.renderer.addClass(this.el.nativeElement.querySelector('.mat-card'), 'scrolled');
+
+
+    } else {
+      this.renderer.removeClass(this.el.nativeElement.querySelector('.mat-card'), 'scrolled');
+
+      this.hideImage = scrollPosition > triggerHeight;
+
+    }
+  }
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit(): void {
+
   }
 
 }
