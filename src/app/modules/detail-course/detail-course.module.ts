3af18434b39e4ae33ac27d0c461af286839c906f
Anderson Ore Aleman <u202011467@upc.edu.pe>
2024-01-22 11:01:02 -0500

    feat(course): add course modules

--- src/app/modules/detail-course/detail-course.module.ts
+++ src/app/modules/detail-course/detail-course.module.ts
@@ -4,15 +4,18 @@ import {DetailCourseComponent} from "./pages/detail-course/detail-course.compone
 import {DetailCourseRoutingModule} from "./detail-course-routing.module";
 import {SharedModule} from "../../shared/shared.module";
 import { DetailCourseCardComponent } from './components/detail-course-card/detail-course-card.component';
+import { ModuleCardComponent } from './components/module-card/module-card.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 
 
 @NgModule({
-  declarations: [DetailCourseComponent, DetailCourseCardComponent],
+  declarations: [DetailCourseComponent, DetailCourseCardComponent, ModuleCardComponent],
   imports: [
     CommonModule,
     DetailCourseRoutingModule,
-    SharedModule
+    SharedModule,
+    MatCheckboxModule
 
   ]
 })
